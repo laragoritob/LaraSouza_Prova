e6c7ac6fa00205ea6f85d6be8eaa82adaf1b9dd8
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-08-11 14:31:58 -0300

    Arquivo Principal
    Responsável pela navegabilidade no CRUD

--- principal.php
+++ principal.php
@@ -3,7 +3,7 @@
     require_once 'conexao.php';
 
     if (!isset($_SESSION['usuario'])) {
-        header('Location: login.php');
+        header('Location: index.php');
         exit();
     }
 
@@ -29,5 +29,60 @@
               "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
               "Alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
               "Excluir"=>["excluir_produto.php"]],
-    ]
+
+        // PERMISSÕES DO ALMOXARIFE
+        3 => ["Cadastrar"=>["cadastro_fornecedor.php", "cadastro_produto.php"],
+              "Buscar"=>["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+              "Alterar"=>["alterar_fornecedor.php", "alterar_produto.php"],
+              "Excluir"=>["excluir_produto.php"]],
+
+        // PERMISSÕES DO CLIENTE
+        4 => ["Cadastrar"=>["cadastro_cliente.php"],
+              "Buscar"=>["buscar_cliente.php"],
+              "Alterar"=>["alterar_cliente.php"]],
+    ];
+
+    // OBTENDO AS OPÇÕES DISPONIVEIS PARA O PERFIL DO USUÁRIO LOGADO
+    $opcoes_menu = $permissoes["$id_perfil"];
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Painel Principal </title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="scripts.js"></script>
+</head>
+<body>
+    <header>
+        <div class="saudacao">
+            <h2> Bem-Vindo, <?php echo $_SESSION['usuario']?>! Perfil: <?php echo $nome_perfil ?> </h2>
+        </div>
+
+        <div class="logout">
+            <form action="logout.php" method="POST">
+                <button type="submit"> Logout </button>
+            </form>
+        </div>
+    </header>
+    <nav>
+        <ul class="menu">
+            <?php foreach($opcoes_menu as $categoria => $arquivos) { ?>
+                <li class="dropdown">
+                    <a href="#"><?= $categoria ?></a>
+
+                    <ul class="dropdown-menu">
+                        <?php foreach($arquivos as $arquivo) { ?>
+                            <li>   
+                                <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </li>
+            <?php } ?>
+        </ul>
+    </nav>
+</body>
+</html>
