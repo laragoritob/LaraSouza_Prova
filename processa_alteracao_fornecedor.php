2f2c06fa1c3627ef7767d8fa08088d53f7294ba4
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-08-21 17:00:27 -0300

    Arquivo Processar Alteração

---
+++ processa_alteracao_fornecedor.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    require_once 'conexao.php';
+
+    // VERIFICA SE O USUÁRIO TEM PERMISSÃO DE ADM
+    if ($_SESSION['perfil'] !=1 && $_SESSION['perfil'] !=2 && $_SESSION['perfil'] !=3) {
+        echo "<script>alert('Acesso Negado!'); window.location.href='principal.php';</script>";
+        exit();
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id_fornecedor = $_POST['id_fornecedor'];
+        $nome_fornecedor = $_POST['nome_fornecedor'];
+        $endereco = $_POST['endereco'];
+        $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        $contato = $_POST['contato'];
+
+        // ATUALIZA OS DADOS DO USUÁRIO
+            $sql = "UPDATE fornecedor SET nome_fornecedor = :nome_fornecedor, endereco = :endereco, telefone = :telefone, email = :email, contato = :contato
+                     WHERE id_fornecedor = :id";
+
+            $stmt = $pdo->prepare($sql);
+        
+
+        $stmt->bindParam(':nome_fornecedor', $nome_fornecedor);
+        $stmt->bindParam(':endereco', $endereco);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':contato', $contato);
+        $stmt->bindParam(':id', $id_fornecedor);
+
+        if ($stmt->execute()) {
+            echo "<script>alert('Usuário atualizado com sucesso!'); window.location.href='buscar_fornecedor.php';</script>";
+        } else {
+            echo "<script>alert('Erro ao atualizar usuário.'); window.location.href='alterar_fornecedor.php?id=$id_fornecedor';</script>";
+        }
+    }
+?>
